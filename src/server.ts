2341e45f3d63f7f864f75cf3e3229eed55fe97c3
Hwrdy <Howard.CHYeh@gmail.com>
2025-04-19 17:19:29 +0800

    add simple MCP

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.ts";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: 'string-plus-server',
+  version: '1.0.0',
+});
+
+server.tool(
+  'add',
+  {
+    numFirst: z.number(),
+    numSecond: z.number(),
+  },
+  async ({ numFirst, numSecond }) => {
+    return {
+      content: [{ type: 'text', text: `The sum is ${numFirst + numSecond}` }],
+    };
+  },
+);
+
+server.resource(
+  'greeting',
+  new ResourceTemplate('greeting://{name}', { list: undefined}),
+  async (uri, { name }) => ({
+    content: [{ uri: uri.href, text: `Hello ${name}` }],
+  }),
+);
+
+const transport = new StdioServerTransport();
+server.connect(transport);
+
+
